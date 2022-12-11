02f497dff31f18f6238f151f7b9b6527bc1cf6de
Praveen kumar verma <vermapraween6543@gmail.com>
2022-12-11 15:27:10 +0530

    refactored LandingPage

--- client/src/pages/LandingPage/LandingPage.js
+++ client/src/pages/LandingPage/LandingPage.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import classes from './LandingPage.module.css';
 import Button from '../../components/Button/Button';
 import { Link } from 'react-router-dom';
@@ -7,51 +7,57 @@ const LandingPage = () => {
   return (
     <main className={classes.main}>
       <div className={classes.leftDiv}>
-        <img src='/Assets/Logo.svg' alt='logo' />
+        <img src='/Assets/Logo.svg' alt='logo' className={classes.logo} />
 
-        <h1>Explore the best camps on Earth.</h1>
-        <p className={classes.description}>
-          YelpCamp is a curated list of best camping spots on Earth. Unfiltered
-          and Unbiased reviews
-        </p>
-        <ul>
-          <li>
-            <img src='/Assets/CheckMark.svg' alt='checkMark' />
-            Add your own camp suggestions.
-          </li>
-          <li>
-            <img src='/Assets/CheckMark.svg' alt='checkMark' />
-            Leave reviews and experiences.
-          </li>
-          <li>
-            <img src='/Assets/CheckMark.svg' alt='checkMark' />
-            See locations for all camps.
-          </li>
-        </ul>
-        <Button className={classes.btn}>
-          <Link className={classes.Link} to='/campgrounds'>
-            View CampGrounds
-          </Link>
-        </Button>
+        <div className={classes.heroDiv}>
+          <img src='/Assets/HeroImage(TabletMobile).jpg' alt='logo' />
+        </div>
 
-        <div className={classes.companiesDiv}>
-          <p>Partnered with:</p>
-          <div className={classes.companies}>
-            <img
-              className={classes.company}
-              src='/Assets/Airbnb.svg'
-              alt='air-bnb'
-            />
-            <img
-              className={classes.company}
-              src='/Assets/Booking.svg'
-              alt='Booking.com'
-            />
-            <img
-              className={classes.company}
-              src='/Assets/PlumGuide.svg'
-              alt='PlumGuide'
-            />
+        <div className={classes.subContainer}>
+          <h1>Explore the best camps on Earth.</h1>
+          <p className={classes.description}>
+            YelpCamp is a curated list of best camping spots on Earth.
+            Unfiltered and Unbiased reviews
+          </p>
+          <ul>
+            <li>
+              <img src='/Assets/CheckMark.svg' alt='checkMark' />
+              Add your own camp suggestions.
+            </li>
+            <li>
+              <img src='/Assets/CheckMark.svg' alt='checkMark' />
+              Leave reviews and experiences.
+            </li>
+            <li>
+              <img src='/Assets/CheckMark.svg' alt='checkMark' />
+              See locations for all camps.
+            </li>
+          </ul>
+          <Button className={classes.btn}>
+            <Link className={classes.Link} to='/campgrounds'>
+              View CampGrounds
+            </Link>
+          </Button>
+
+          <div className={classes.companiesDiv}>
+            <p>Partnered with:</p>
+            <div className={classes.companies}>
+              <img
+                className={classes.company}
+                src='/Assets/Airbnb.svg'
+                alt='air-bnb'
+              />
+              <img
+                className={classes.company}
+                src='/Assets/Booking.svg'
+                alt='Booking.com'
+              />
+              <img
+                className={classes.company}
+                src='/Assets/PlumGuide.svg'
+                alt='PlumGuide'
+              />
+            </div>
           </div>
         </div>
       </div>
