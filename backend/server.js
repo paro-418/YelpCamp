56bc643662dab92bf59c76af71bccd0b7b5b9e33
Praveen kumar verma <vermapraween6543@gmail.com>
2022-12-20 14:54:14 +0530

    build complete

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
@@ -8,7 +8,7 @@ const authRouter = require('./Routes/AuthRoute');
 const campGroundRouter = require('./Routes/CampGroundRoutes');
 const reviewRouter = require('./Routes/ReviewRoutes');
 app.use(cors());
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(express.json());
 
 app.use('/auth', authRouter);
@@ -16,6 +16,14 @@ app.use('/campgrounds', campGroundRouter);
 app.use('/campgrounds/review', reviewRouter);
 
 connectToDatabase();
+
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+  app.get('/', (req, res) => {
+    app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 const server = app.listen(process.env.PORT, () => {
   console.log(`Server Listening on port ${process.env.PORT}`);
 });
