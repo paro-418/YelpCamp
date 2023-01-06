fbe409d4e2daed46253df80266e0c10fb176a3a5
Praveen kumar verma <vermapraween6543@gmail.com>
2023-01-06 21:13:28 +0530

    renamed MONGO_URL TO MONGO_URI

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
 module.exports = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
